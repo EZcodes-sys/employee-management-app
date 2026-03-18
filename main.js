725ce09ca36086f3a41afd7ce985ec0dae30189f
EZcodes-sys <researchproject36@gmail.com>
2026-03-18 11:52:20 +0300

    Add main.js with alert and confirm functions for actions

---
+++ main.js
@@ -0,0 +1,11 @@
+function showDetails() {
+    alert("Employee full details will appear here.");
+}
+
+function editContact() {
+    alert("Edit employee feature.");
+}
+
+function deleteContact() {
+    confirm("Are you sure you want to delete?");
+}
