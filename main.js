2e36962262f8636caaaa07429f7c398c11eaeeeb
EZcodes-sys <researchproject36@gmail.com>
2026-03-24 23:12:35 +0300

    Add dark mode toogle

--- main.js
+++ main.js
@@ -126,4 +126,22 @@ if (activityList) {
     }
 }
 
+
+});
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "enabled");
+    } else {
+        localStorage.setItem("darkMode", "disabled");
+    }
+}
+
+// LOAD MODE
+document.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.getItem("darkMode") === "enabled") {
+        document.body.classList.add("dark-mode");
+    }
 });
